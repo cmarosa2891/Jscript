2749594d38e519c07abf79b4eaa7a5726eaeeb0b
cmarosa2891 <cmarosa@gmail.com>
2022-07-07 09:39:40 -0300

    Finalizacion cargarautos.js

--- cargarautos.js
+++ cargarautos.js
@@ -15,19 +15,25 @@ const camion = {
 
     cargarauto(pesoauto, patente) {
 
-        if ((pesoauto + this.pesocarga < this.pesom)) {   //patente !== this.patentes.find(patente) && 
+        if ((pesoauto + this.pesocarga < this.pesom) && (!this.patentes.includes(patente))) {   //patente !== this.patentes.find(patente) && 
             alert(`Se procederá con la carga del auto patente: ${patente} peso: ${pesoauto} `)
             this.pesocarga = this.pesocarga + pesoauto
             this.patentes.push(patente)
-        } else { alert(`No se procederá con la carga. Carga actual ${this.pesocarga} peso vehículo: ${pesoauto} peso máximo: ${this.pesom} - Excedente: ${this.pesom - this.pesocarga + pesoauto}`)}
+        } else {
+            if (this.patentes.includes(patente)) {
+                alert('vehículo ya cargado')
+            } else {
+                alert(`No se procederá con la carga. Carga actual ${this.pesocarga} peso vehículo: ${pesoauto} peso máximo: ${this.pesom} - Excedente: ${this.pesom - this.pesocarga + pesoauto}`)
+            }
+        }
     },
 
     listaautos() {
         console.log(this.patentes)
     },
 
-    consultapantentes(patverificar){
-        if(patverificar === this.patentes.find(patverificar)){
+    consultapantentes(patverificar) {
+        if (this.patentes.includes(patverificar)) {
             alert('vehículo ya cargado')
         } else {
             alert('vehículo no encontrado')
